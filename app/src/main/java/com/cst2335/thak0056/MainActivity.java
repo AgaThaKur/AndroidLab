cb33e02ff95efa65fb915eb76818690be97294a9
Agam Thakur <thak0056@algonquinlive.com>
2023-02-09 08:36:18 -0500

    Finished lab 3

--- app/src/main/java/com/cst2335/thak0056/MainActivity.java
+++ app/src/main/java/com/cst2335/thak0056/MainActivity.java
@@ -1,21 +1,26 @@
 package com.cst2335.thak0056;
 
+import static android.provider.ContactsContract.Intents.Insert.EMAIL;
+import static java.lang.System.in;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-private Button button;
-private Switch sw;
+//private Button button;
+//private Switch sw;
 
 
     @SuppressLint("MissingInflatedId")
@@ -23,7 +28,7 @@ private Switch sw;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_constraint);
-        button = findViewById(R.id.button2);
+       /* button = findViewById(R.id.button2);
         sw=findViewById(R.id.switch1);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -48,4 +53,15 @@ private Switch sw;
                 snackbar.show();
             }
         } );
-        }}
+        */
+     Button log = findViewById(R.id.login);
+     TextView tex = findViewById(R.id.email);
+     log.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+             goToProfile.putExtra("EMAIL",tex.getText().toString());
+             startActivity(goToProfile);
+         }
+     });
+    }}
---
+++ app/src/main/java/com/cst2335/thak0056/ProfileActivity.java
@@ -0,0 +1,95 @@
+package com.cst2335.thak0056;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+    public static final String TAG = "PROFILE_ACTIVITY";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Button but = findViewById(R.id.button);
+        TextView emailEditText = findViewById(R.id.emailpa);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+        Intent fromMain = getIntent();
+
+        emailEditText.setText( fromMain.getStringExtra("EMAIL"));
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            myPictureTakerLauncher.launch(takePictureIntent);
+        }
+    }
+
+    ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
+
+            new ActivityResultContracts.StartActivityForResult()
+            ,new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    ImageView imgView = findViewById(R.id.imageView);
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
+                        imgView.setImageBitmap(imgbitmap);
+                    }
+                    else if(result.getResultCode() == Activity.RESULT_CANCELED)
+                        Log.i(TAG, "User refused to capture a picture.");
+                }
+            } );
+    @Override
+
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "In function: onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "In function: onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "In function: onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "In function: onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "In function: onDestroy");
+}
+
+}
